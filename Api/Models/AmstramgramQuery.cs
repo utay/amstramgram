905a2eb93fcd5732fc448c905c4f9eff7b8a03fa
Yannick Utard <yannickutard@gmail.com>
2018-05-09 00:30:36 +0200

    back: add picture endpoint

--- Api/Models/AmstramgramQuery.cs
+++ Api/Models/AmstramgramQuery.cs
@@ -8,7 +8,7 @@ namespace Api.Models
     {
         public AmstramgramQuery() { }
 
-        public AmstramgramQuery(Core.Data.IUserRepository userRepository, IMapper mapper)
+        public AmstramgramQuery(Core.Data.IUserRepository userRepository, Core.Data.IPictureRepository pictureRepository, IMapper mapper)
         {
             Name = "Query";
 
@@ -22,6 +22,20 @@ namespace Api.Models
                     var id = context.GetArgument<int>("id");
                     var user = userRepository.Get(id).Result;
                     var mapped = mapper.Map<User>(user);
+                    return mapped;
+                }
+            );
+
+            Field<PictureType>(
+                "picture",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "id", Description = "id of the picture" }
+                ),
+                resolve: context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    var picture = pictureRepository.Get(id).Result;
+                    var mapped = mapper.Map<Picture>(picture);
                     return mapped;
                 }
             );
