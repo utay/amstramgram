0bf9af25914b96a6f40f53c4da946fe383799109
Loïc Ferreira <loicferreira.ds@gmail.com>
2018-05-12 17:33:35 +0200

    fb-connect: Add Service to get account information from Access Token

---
+++ Facebook/Service/FacebookService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook.Client;
+using Facebook.Data;
+
+namespace Facebook.Service
+{
+    public class FacebookService : IFacebookService
+    {
+        private readonly IFacebookClient _facebookClient;
+
+        public FacebookService(IFacebookClient facebookClient)
+        {
+            _facebookClient = facebookClient;
+        }
+
+        public async Task<Account> GetAccountAsync(string accessToken)
+        {
+            var result = await _facebookClient.GetAsync<dynamic>(
+                accessToken, "me", "fields=id,name,email,first_name,last_name,age_range,birthday,gender,locale");
+
+            if (result == null)
+            {
+                return new Account();
+            }
+
+            var account = new Account
+            {
+                Id = result.id,
+                Email = result.email,
+                Name = result.name,
+                UserName = result.username,
+                FirstName = result.first_name,
+                LastName = result.last_name,
+                Locale = result.locale
+            };
+
+            return account;
+        }
+
+        public async Task PostOnWallAsync(string accessToken, string message)
+        {
+            await _facebookClient.PostAsync(accessToken, "me/feed", new { message });
+        }
+    }
+}
