1e3b6056ec2386acf0d931aa0bcd718a6f42eaee
Loïc Ferreira <loicferreira.ds@gmail.com>
2018-05-12 17:38:14 +0200

    fb-connect: add controllers for connection

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace Api.Controllers
+{
+    [Authorize]
+    [Route("users")]
+    public class AccountController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public AccountController(ILogger logger = null)
+        {
+            _logger = logger;
+        }
+
+        //
+        // GET: /Account/Login
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("sign_in")]
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            // Clear the existing external cookie to ensure a clean login process
+            _logger?.LogInformation("Connection to Facebook...");         
+            Facebook.Client.FacebookClient client = new Facebook.Client.FacebookClient();
+            string accessToken = await Task.Run(() => client.GetAccessToken());
+            _logger?.LogInformation($"Facebook accessToken: { accessToken }");
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+           return await Task.Run(() => RedirectToAction(nameof(HomeController.Index), "Home"));
+        }
+
+        //
+        // POST: /Account/ExternalLogin
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public IActionResult ExternalLogin(string provider, string returnUrl = null)
+        {
+            // Request a redirect to the external login provider.            
+            return View();
+        }
+
+        //
+        // GET: /Account/ExternalLoginCallback
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            await Task.Run(() => Console.WriteLine("ExternalLoginCallback"));
+            return View();
+        }
+
+        //
+        // GET /Account/AccessDenied
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        #endregion
+    }
+}
